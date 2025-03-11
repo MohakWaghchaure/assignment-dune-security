d62f048a43eeb3afe70b2e2259473576a7704b0f
Mohak Waghchaure <mohak11jan@gmail.com>
2025-03-11 02:21:54 -0400

    components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import EmployeeTable from './components/EmployeeTable';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="body-wrapper">
+        <div className="card">
+          <h1>Testing</h1>
+          <EmployeeTable></EmployeeTable>
+        </div>
+      </div>
     </div>
   );
 }
